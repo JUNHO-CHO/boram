cdf03d772d993aff150850d3bbb762d97bf065d2
junho <willight1@naver.com>
2024-08-21 10:52:21 +0900

    project name change

--- boram/accounts/views.py
+++ boram/accounts/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
 from django.views.decorators.http import require_POST, require_http_methods
+from django.contrib.auth.forms import UserCreationForm
 
 
 @require_http_methods(['GET', 'POST'])
@@ -24,3 +25,14 @@ def logout(request):
     if request.user.is_authenticated:
         auth_logout(request)
     return redirect("index")
+
+def signup(request):
+    if request.method =="POST":
+        form = UserCreationForm(request.POST)
+        if form. is_valid():
+            form.save()
+            return redirect("index")
+    else:
+        form = UserCreationForm()
+    context = {"form":form}
+    return render(request, "accounts/signup.html", context)
