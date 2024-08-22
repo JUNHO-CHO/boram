f16119e7811f455f916c2b2742426e0f8c270983
nahyeons <skgus3721@gmail.com>
2024-08-22 14:33:45 +0900

    articles

--- boram/articles/urls.py
+++ boram/articles/urls.py
@@ -5,4 +5,8 @@ from . import views
 app_name="articles"
 urlpatterns = [
     path("index/", views.index, name = "index"),
+    path("",views.articles, name="articles"),
+    path("<int:pk>/", views.article_detail, name="article_detail"),
+    path("new/",views.new, name="new"),
+    path("<int:pk>/delete/", views.delete, name="delete"),
 ]
--- boram/articles/views.py
+++ boram/articles/views.py
@@ -1,6 +1,29 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .models import Article
 
 # Create your views here.
 def index(request):
 	return render(request, "articles/index.html")
+
+def articles(request):
+    articles = Article.objects.all().order_by("-created_at")
+    context = {
+        "articles": articles,
+    }
+    return render(request, "articles.html", context)
+
+def article_detail(request, pk):
+    article = Article.objects.get(pk=pk)
+    context = {"article": article,}
+    return render(request, "article_detail.html", context)
+
+def new(request):
+    return render(request, "new.html")
+
+def delete(request, pk):
+    # 글 삭제
+    article = Article.objects.get(pk=pk)
+    article.delete()
+    # 글 삭제하면 목록으로 돌아감
+    return redirect("articles")
