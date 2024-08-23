987398e1544e69093fe13895265833322f183fe4
minseong <jeonminseong070300@gmail.com>
2024-08-23 20:22:50 +0900

    profile update user page  base page

--- boram/accounts/forms.py
+++ boram/accounts/forms.py
@@ -7,24 +7,27 @@ class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = get_user_model()
         fields = UserCreationForm.Meta.fields + ()
+        exclude = ["followings"]
 
 
 class CustomUserChangeForm(UserChangeForm):
 
     class Meta:
         model = get_user_model()
+        # fields = "__all__"
+        # exclude = ["password", "last_login", "is_superuser", "is_staff", "is_active", "date_joined", "following", "Groups", "User permissions"]
         fields = [
+            # "username",
             "first_name",
             "last_name",
             "email",
+            "image",
         ]
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-
         if self.fields.get("password"):
             password_help_text = (
                 "You can change the password " '<a href="{}">here</a>.'
             ).format(f"{reverse('accounts:change_password')}")
             self.fields["password"].help_text = password_help_text
-            
--- boram/accounts/models.py
+++ boram/accounts/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    pass
-    # followings = models.ManyToManyField(
-    #     "self", related_name="followers", symmetrical=False
-    # )
+    image = models.ImageField(upload_to="images/", blank=True, null=True)
+    followings = models.ManyToManyField(
+        "self", related_name="followers", symmetrical=False
+    )
