d30a49bc0e7aae86cbc8698c24a677d30c27544c
mesurechiffon <dri0205@naver.com>
2024-08-23 09:40:10 +0900

    edit accounts

--- boram/accounts/views.py
+++ boram/accounts/views.py
@@ -6,10 +6,12 @@ from django.contrib.auth.forms import (
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
 from django.views.decorators.http import require_POST, require_http_methods
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import ( 
+    UserCreationForm,
+    UserChangeForm,
+)
 from django.contrib.auth import update_session_auth_hash
 from .forms import CustomUserChangeForm, CustomUserCreationForm
-
 
 
 @require_http_methods(['GET', 'POST'])
@@ -36,13 +38,13 @@ def logout(request):
 @require_http_methods(['GET','POST'])
 def signup(request):
     if request.method =="POST":
-        form = UserCreationForm(request.POST)
+        form = CustomUserCreationForm(request.POST)
         if form. is_valid():
             user=form.save()
             auth_login(request, user)
             return redirect("articles:index")
     else:
-        form = UserCreationForm()
+        form = CustomUserCreationForm()
     context = {"form":form}
     return render(request, "accounts/signup.html", context)
 
@@ -52,7 +54,7 @@ def delete(request):
     if request.user.is_authenticated:
         request.user.delete()
         auth_logout(request)
-    return redirect("index")
+    return redirect("articles:index")
 
 
 @require_http_methods(["GET", "POST"])
@@ -61,7 +63,7 @@ def update(request):
         form = CustomUserChangeForm(request.POST, instance=request.user)
         if form.is_valid():
             form.save()
-            return redirect("index")
+            return redirect("articles:index")
     else:
         form = CustomUserChangeForm(instance=request.user)
     context = {"form": form}
@@ -74,7 +76,7 @@ def change_password(request):
         if form.is_valid():
             form.save()
             update_session_auth_hash(request, form.user)
-            return redirect("index")
+            return redirect("articles:index")
     else:
         form = PasswordChangeForm(request.user)
     context = {"form": form}
