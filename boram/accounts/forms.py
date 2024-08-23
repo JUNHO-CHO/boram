b9895e3b34702b27dab9fc6f90dbb0df8b14d7c7
mesurechiffon <dri0205@naver.com>
2024-08-23 15:15:42 +0900

    feat: 회원 탈퇴 시 비밀번호 입력, 체크박스

--- boram/accounts/forms.py
+++ boram/accounts/forms.py
@@ -1,4 +1,9 @@
-from django.contrib.auth.forms import UserChangeForm, UserCreationForm
+from django import forms
+from django.contrib.auth.forms import (
+    UserChangeForm,
+    UserCreationForm,
+    AuthenticationForm
+)
 from django.contrib.auth import get_user_model
 from django.urls import reverse
 
@@ -27,4 +32,23 @@ class CustomUserChangeForm(UserChangeForm):
                 "You can change the password " '<a href="{}">here</a>.'
             ).format(f"{reverse('accounts:change_password')}")
             self.fields["password"].help_text = password_help_text
-            
+
+
+class PasswordConfirmationForm(AuthenticationForm):
+
+    agree = forms.BooleanField(
+        label="탈퇴를 진행하겠습니다.",
+        required=True,  # 체크박스를 필수로 설정
+        error_messages={
+            'required': '계정 삭제에 동의하려면 체크해야 합니다.'
+        }
+    )
+
+    class Meta:
+        model = get_user_model()
+        fields = ["password"]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields.pop('username')
+        self.fields['password'].label = "비밀번호"
