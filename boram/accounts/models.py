88b2583f2596a9ff4ea4c7eaa1ce1f43da50fd70
mesurechiffon <dri0205@naver.com>
2024-08-27 02:11:53 +0900

    fix: signup-image, design: article_detail.html, create.html, update.html

--- boram/accounts/models.py
+++ boram/accounts/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    image = models.ImageField(upload_to="images/", blank=True, null=True)
+    image = models.ImageField(upload_to="images/", blank=True, null=True, default='images/default.png')
     followings = models.ManyToManyField(
         "self", related_name="followers", symmetrical=False
     )
